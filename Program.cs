9b3393a4befc6a9f8e78692450a9f28531fa5ac2
huseynstudent <huseynstudent5628@gmail.com>
2025-06-12 16:39:55 +0400

    json error fixed,switced choice to string to avoid error

--- Program.cs
+++ Program.cs
@@ -1,38 +1,35 @@
 ﻿using System;
-using C__Json;
-using C__Json.Models;
-using C__Json.Abstract;
 using C__Json.Services;
+
 class Program
 {
     static void Main(string[] args)
     {
         bool hasExited = false;
-        string File = "Students.json";
+        StudentService studentService = new StudentService();
 
         while (!hasExited)
         {
-            Console.WriteLine("Enter a command \n1)add student\t2)remove student\t3)show all students: ");
+            Console.WriteLine("Enter a command \n1)add student\t2)remove student\t3)show all students\n\t'exit' to quit:");
             string input = Console.ReadLine();
-            if (input.ToLower() == "exit")
+
+            switch (input.ToLower())
             {
-                hasExited = true;
-            }
-            else if (input == "1")
-            {
-                AddStudent();
-            }
-            else if (input == "2")
-            {
-                RemoveStudents();
-            }
-            else if (input == "3")
-            {
-                ShowAllStudents();
-            }
-            else
-            {
-                Console.WriteLine("Invalid command. Please try again.");
+                case "exit":
+                    hasExited = true;
+                    break;
+                case "1":
+                    studentService.AddStudent();
+                    break;
+                case "2":
+                    studentService.RemoveStudents();
+                    break;
+                case "3":
+                    studentService.ShowAllStudents();
+                    break;
+                default:
+                    Console.WriteLine("Invalid command. Please try again.");
+                    break;
             }
         }
     }
